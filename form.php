fa72842013238b6bf924802b7474a148f6e784d3
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-15 15:00:25 +0100

    inserimento paragrafo e collegamento alla pagina results

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <h1>
+        Inserire il paragrafo o la parola da censurare:
+    </h1>
+    <form action="results.php">
+        <div>
+            <label for="paragraph">Paragrafo:</label>
+            <input type="text" placeholder="Inserisci un paragrafo">
+        </div>
+    </form>
+</body>
+</html>
