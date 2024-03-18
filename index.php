adaf3bea474be75d3fb3f0e1f39476c3eef43e2c
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-18 15:15:28 +0100

    correzione esercizio

--- index.php
+++ index.php
@@ -9,11 +9,16 @@
     <h1>
         Inserire il paragrafo o la parola da censurare:
     </h1>
-    <form action="results.php">
+    <form action="results.php" method="get">
         <div>
             <label for="paragraph">Paragrafo:</label>
-            <input type="text" placeholder="Inserisci un paragrafo">
+            <textarea id="paragraph" name="paragraph" rows="4" cols="50"></textarea>
         </div>
+        <div>
+            <label for="word">Parola:</label>
+            <input id="word" name="word" type="text" placeholder="Inserisci una parola">
+        </div>
+        <input type="submit" value="Vai alla prossima pagina">
     </form>
 </body>
 </html>
--- results.php
+++ results.php
@@ -1,3 +1,17 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+    $paragraph = $_GET['paragraph'];
+
+    $word = $_GET['word'];
+
+    $censored = str_replace($word, '***', $paragraph);
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -7,5 +21,45 @@
 </head>
 <body>
     
+    <div>
+        <h2>
+            Paragrafo iniziale
+        </h2>
+        <p>
+            <?php
+
+                echo $paragraph;
+
+            ?>
+        </p>
+        <h3>
+            Lunghezza Paragrafo: <?php echo strlen($paragraph); ?> caratteri.
+        </h3>
+    </div>
+    <div>
+        <h2>
+            Parola Iniziale
+        </h2>
+        <p>
+        <?php
+
+            echo $word;
+
+        ?>
+        </p>
+    </div>
+    <div>
+        <h2>
+            Paragrafo Censurato:
+        </h2>
+        <p>
+            <?php echo ($censored) ?>
+        </p>
+        <p>
+            Lunghezza <?php echo strlen($censored) ?>
+        </p>
+    </div>
+    
+
 </body>
 </html>
